a167ac2ee8f68590a968d0019b24f8a8b8379fac
CRE4K <65049028+cre4k@users.noreply.github.com>
2023-05-03 10:00:24 +0100

    doing the needful even kinderer

--- src/cmds/core/fun.py
+++ src/cmds/core/fun.py
@@ -52,7 +52,7 @@ class Fun(Cog):
 
     @slash_command(guild_ids=settings.guild_ids, name="cre4k", default_permission=True)
     @cooldown(1, 60, BucketType.user)
-    async def start_here(self, ctx: ApplicationContext) -> Interaction | WebhookMessage:
+    async def cre4k(self, ctx: ApplicationContext) -> Interaction | WebhookMessage:
         """cre4k"""
         return await ctx.respond(
             "https://gfycat.com/ringedunlawfulfairyfly"
