6a62432504cdc59fe749e92893cdb3187f7fa1ea
Dimosthenis Schizas <dimos@hackthebox.eu>
2023-11-09 17:42:22 +0200

    Add role academy CDSA in .test.env and update the verify command to use async/await syntax

--- src/cmds/core/verify.py
+++ src/cmds/core/verify.py
@@ -9,6 +9,7 @@ from discord.ext.commands import cooldown
 from src.bot import Bot
 from src.core import settings
 from src.helpers.verification import process_certification
+
 logger = logging.getLogger(__name__)
 
 
@@ -19,27 +20,30 @@ class VerifyCog(commands.Cog):
         self.bot = bot
 
     @slash_command(
-        guild_ids=settings.guild_ids,
-        description="Verify your HTB Certifications!"
+        guild_ids=settings.guild_ids, description="Verify your HTB Certifications!"
     )
     @cooldown(1, 60, commands.BucketType.user)
-    async def verifycertification(self, ctx: ApplicationContext, certid: str, fullname: str) -> Interaction | WebhookMessage:
+    async def verifycertification(
+        self, ctx: ApplicationContext, certid: str, fullname: str
+    ) -> Interaction | WebhookMessage:
+        """Verify HTB Academy certificate."""
         if not certid or not fullname:
-            ctx.respond("You must supply a cert id!")
+            await ctx.respond("You must supply a cert id!")
             return
         if not certid.startswith("HTBCERT-"):
-            ctx.respond("CertID must start with HTBCERT-")
+            await ctx.respond("CertID must start with HTBCERT-")
             return
-        cert = process_certification(certid, fullname)
+        cert = await process_certification(certid, fullname)
         if cert:
-            toAdd = settings.get_cert(cert)
-            ctx.author.add_roles(toAdd)
-            ctx.respond(f"Added {cert}!")
+            to_add = settings.get_cert(cert)
+            await ctx.author.add_roles(to_add)
+            await ctx.respond(f"Added {cert}!")
         else:
-            ctx.respond("Unable to find certification with provided details")
+            await ctx.respond("Unable to find certification with provided details")
+
     @slash_command(
         guild_ids=settings.guild_ids,
-        description="Receive instructions in a DM on how to identify yourself with your HTB account."
+        description="Receive instructions in a DM on how to identify yourself with your HTB account.",
     )
     @cooldown(1, 60, commands.BucketType.user)
     async def verify(self, ctx: ApplicationContext) -> Interaction | WebhookMessage:
@@ -51,7 +55,9 @@ class VerifyCog(commands.Cog):
         embed_step1.add_field(
             name="Step 1: Log in at Hack The Box",
             value="Log in to your Hack The Box account at <https://www.hackthebox.com/> and navigate to the settings "
-                  "page.", inline=False, )
+            "page.",
+            inline=False,
+        )
         embed_step1.set_image(
             url="https://media.discordapp.net/attachments/724587782755844098/839871275627315250/unknown.png"
         )
@@ -61,7 +67,9 @@ class VerifyCog(commands.Cog):
         embed_step2.add_field(
             name="Step 2: Locate the Account Identifier",
             value='In the settings tab, look for a field called "Account Identifier". Next, click the green button to '
-                  "copy your secret identifier.", inline=False, )
+            "copy your secret identifier.",
+            inline=False,
+        )
         embed_step2.set_image(
             url="https://media.discordapp.net/attachments/724587782755844098/839871332963188766/unknown.png"
         )
@@ -71,7 +79,8 @@ class VerifyCog(commands.Cog):
         embed_step3.add_field(
             name="Step 3: Identification",
             value="Now type `/identify IDENTIFIER_HERE` in the bot-commands channel.\n\nYour roles will then be "
-                  "automatically applied.", inline=False
+            "automatically applied.",
+            inline=False,
         )
         embed_step3.set_image(
             url="https://media.discordapp.net/attachments/709907130102317093/904744444539076618/unknown.png"
@@ -92,7 +101,9 @@ class VerifyCog(commands.Cog):
             return await ctx.respond(
                 "An unexpected error happened (HTTP 400, bad request). Please contact an Administrator."
             )
-        return await ctx.respond("Please check your DM for instructions.", ephemeral=True)
+        return await ctx.respond(
+            "Please check your DM for instructions.", ephemeral=True
+        )
 
 
 def setup(bot: Bot) -> None:
