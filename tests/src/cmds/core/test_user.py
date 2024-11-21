24b2b86c47aa1b1b40f0c2eeb4d7d3653cdb38bf
Jelle Janssens <janssensjelle@users.noreply.github.com>
2024-11-21 17:38:14 +0100

    Update test_user.py according feedback
    Co-authored-by: Dimosthenis Schizas <dimoschi@gmail.com>

--- tests/src/cmds/core/test_user.py
+++ tests/src/cmds/core/test_user.py
@@ -20,8 +20,10 @@ class TestUserCog:
         user_to_kick.send = AsyncMock()
         user_to_kick.name = "User to Kick"
 
-        with patch('src.cmds.core.user.add_evidence_note', new_callable=AsyncMock) as add_evidence_mock, \
-                patch('src.cmds.core.user.member_is_staff', return_value=False):
+        with (
+            patch('src.cmds.core.user.add_evidence_note', new_callable=AsyncMock) as add_evidence_mock, 
+            patch('src.cmds.core.user.member_is_staff', return_value=False
+        ):
             cog = user.UserCog(bot)
             await cog.kick.callback(cog, ctx, user_to_kick, "Violation of rules")
 
