65b3fc8ede936a67a621689dff3c5154f6660bd5
Jelle Janssens <janssensjelle@users.noreply.github.com>
2025-01-25 08:11:41 +0000

    Changed infraction message to be more like the ban message

--- tests/src/cmds/core/test_user.py
+++ tests/src/cmds/core/test_user.py
@@ -31,7 +31,7 @@ class TestUserCog:
 
             reason = "Violation of rules"
             add_infraction_mock.assert_called_once_with(
-                ctx.guild, user_to_kick, 0, f"{ctx.user.name} was kicked on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} for {reason} - Evidence: None", ctx.user
+                ctx.guild, user_to_kick, 0, f"Previously kicked on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} for {reason} - Evidence: None", ctx.user
             )
 
             # Assertions
