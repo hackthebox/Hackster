1e3d1f413d9079bf4e3da47120d1067e40de355c
Dimosthenis Schizas <dimos@hackthebox.eu>
2025-05-07 15:27:13 +0300

    ➕ migrate from uvicorn to hypercorn

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,7 @@ import logging
 from src.bot import bot
 from src.core import settings
 from src.utils.extensions import walk_extensions
-from src.webhooks.server import server
+from src.webhooks.server import serve
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +16,6 @@ for ext in walk_extensions():
 if __name__ == "__main__":
     logger.info("Starting bot & webhook server")
     logger.debug(f"Starting webhook server listening on port: {settings.WEBHOOK_PORT}")
-    bot.loop.create_task(server.serve())
+    bot.loop.create_task(serve())
     logger.debug(f"Starting bot with token: {settings.bot.TOKEN}")
     bot.loop.create_task(bot.run(settings.bot.TOKEN))
