04d85b7b6ee43564f23f4002a70fd7679877979c
0xry4n <ry4n@hackthebox.eu>
2025-06-17 12:20:16 -0400

    ✨ Add optional code parameter to simple response for easier checking of response type

--- src/helpers/responses.py
+++ src/helpers/responses.py
@@ -4,9 +4,10 @@ import json
 class SimpleResponse(object):
     """A simple response object."""
 
-    def __init__(self, message: str, delete_after: int | None = None):
+    def __init__(self, message: str, delete_after: int | None = None, code: str | None = None):
         self.message = message
         self.delete_after = delete_after
+        self.code = code
 
     def __str__(self):
         return json.dumps(dict(self), ensure_ascii=False)
