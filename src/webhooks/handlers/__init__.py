f0655afaebc4f4655d1acadf69979f671579661b
0xry4n <ry4n@hackthebox.eu>
2025-07-16 23:40:32 -0400

    ✨ Add new handlers to handler dict

--- src/webhooks/handlers/__init__.py
+++ src/webhooks/handlers/__init__.py
@@ -2,10 +2,15 @@ from discord import Bot
 from typing import Any
 
 from src.webhooks.handlers.account import AccountHandler
+from src.webhooks.handlers.academy import AcademyHandler
 from src.webhooks.handlers.mp import MPHandler
 from src.webhooks.types import Platform, WebhookBody
 
-handlers = {Platform.ACCOUNT: AccountHandler().handle, Platform.MAIN: MPHandler().handle}
+handlers = {
+    Platform.ACCOUNT: AccountHandler().handle,
+    Platform.MAIN: MPHandler().handle,
+    Platform.ACADEMY: AcademyHandler().handle,
+}
 
 
 def can_handle(platform: Platform) -> bool:
